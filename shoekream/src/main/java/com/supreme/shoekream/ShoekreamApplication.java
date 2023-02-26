13daecdf6789b42d472b5d6fd4c06512e57a11f0
yunanjk <lyra082172@naver.com>
2023-02-26 21:29:07 +0900

    aws 배포 에러 수정중

--- shoekream/src/main/java/com/supreme/shoekream/ShoekreamApplication.java
+++ shoekream/src/main/java/com/supreme/shoekream/ShoekreamApplication.java
@@ -2,10 +2,11 @@ package com.supreme.shoekream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ShoekreamApplication {
 
 	public static void main(String[] args) {
