6aa5160c544fb860c3321e7f6ee2a86b091fcb81
yunanjk <lyra082172@naver.com>
2023-02-26 21:45:47 +0900

    aws 오류 수정중2

--- shoekream/src/main/java/com/supreme/shoekream/ShoekreamApplication.java
+++ shoekream/src/main/java/com/supreme/shoekream/ShoekreamApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class ShoekreamApplication {
 
 	public static void main(String[] args) {
